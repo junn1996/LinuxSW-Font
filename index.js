cb1e35e75173964e14bbf874059b452f5f2fc26a
flod3 <3195666477@qq.com>
2019-01-24 17:00:48 +0800

    cao

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { DatePicker } from 'antd';
 
-function App() {
-  return (
+class App extends React.Component {
+  render(){
+    return (
     <div style={{ margin: 100 }}>
       <h1>AntDesign Demo</h1>
       <hr /><br />
       <DatePicker />
     </div>
   );
+  }
+  
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
